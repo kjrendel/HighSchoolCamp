46d1e6817a78c16b8c9e0daba53e2529df68fcc2
Kaly Rendel <kjrendel@gmail.com>
2019-06-12 11:47:39 -0500

    Math functions

---
+++ Test.py
@@ -0,0 +1,6 @@
+"""
+title: Test
+author: Kaly
+date: 2019-06-12 11:06
+"""
+Print("Test")
---
+++ method_practice.py
@@ -0,0 +1,15 @@
+"""
+title: method_practice
+author: Kaly
+date: 2019-06-12 11:22
+"""
+
+def Age_Calculator(birth_year, current_year):
+    return birth_year - current_year
+
+print (Age_Calculator(2006,1940))
+
+def Avg_Numbers(n1,n2,n3):
+    return (n1+n2+n3)/3
+
+print(Avg_Numbers(6,2,8))
