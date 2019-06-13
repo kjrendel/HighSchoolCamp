0162bea5ac8f10e23a5b86018ccf24d5ce40ce7d
Kaly Rendel <kjrendel@gmail.com>
2019-06-13 15:29:47 -0500

    Added more files

---
+++ data_Structure.py
@@ -0,0 +1,41 @@
+"""
+title: data_Structure
+author: Kaly
+date: 2019-06-13 11:32
+"""
+#shakeball
+# import random
+#
+#
+# def shake_ball():
+#     inp = input("ask a question")
+#     ans = ("yes definitely","without a doubt",'very doubtful','ask again later',"Don't count on it","Cannot Predict now",\
+#            "as i see it, yes","No chance in H E double hockey sticks","Literally just no","Come back later I'm busy",\
+#            "I mean maybe", "I hope so")
+#     return ans[random.randint(0,len(ans))]
+# print(shake_ball())
+#number1
+# numbers = [89, 41, 73, 90]
+# total = 0
+# for i in numbers:
+#      total += i
+#      print(total)
+#number2
+# x = list(range(0, 15, 5))
+# print(x)
+#number3
+# x = list(range(100, 210, 10))
+# print(x)
+#number4
+# x = list(range(80, 32, -8))
+# print(x)
+#number5
+# for i in range(3):
+#     print('Alright')
+
+# #countdown
+# countdown_number = 10
+# while countdown_number>0:
+#     print(countdown_number, end=" ")
+#     countdown_number-=1
+
